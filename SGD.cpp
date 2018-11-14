daf3df29ec323df7b4ff6eff9543b9d47dd08857
Vibhatha Abeykoon <vibhatha@gmail.com>
2018-11-14 13:58:07 -0500

    testing memory leaks in SGD.cpp' '

--- SGD.cpp
+++ SGD.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-SGD::SGD(double **Xn, double* yn, double alphan, int itrN) {
+SGD::SGD(double **Xn, double *yn, double alphan, int itrN) {
     X = Xn;
     y = yn;
     alpha = alphan;
@@ -37,20 +37,21 @@ void SGD::sgd() {
     util.print1DMatrix(wInit, features);
     Matrix matrix(features);
     w = wInit;
-    double* res = new double[features];
+    double *res = new double[features];
     for (int i = 0; i < iterations; ++i) {
-        if(i%10 == 0) {
+        if (i % 10 == 0) {
             cout << "Iteration " << i << "/" << iterations << endl;
         }
         for (int j = 0; j < trainingSamples; ++j) {
             double yixiw = matrix.dot(X[j], w) * y[j];
             //cout << i << ", " << yixiw << endl;
-            if(yixiw<1) {
-                double* xiyia = matrix.scalarMultiply(matrix.subtract(w,matrix.scalarMultiply(X[j], y[j], res), res), alpha, res);
+            if (yixiw < 1) {
+                double *xiyia = matrix.scalarMultiply(matrix.subtract(w, matrix.scalarMultiply(X[j], y[j], res), res),
+                                                      alpha, res);
                 w = matrix.subtract(w, xiyia, res);
             } else {
-                double* wa = matrix.scalarMultiply(w,alpha, res);
-                w = matrix.subtract(w,wa, res);
+                double *wa = matrix.scalarMultiply(w, alpha, res);
+                w = matrix.subtract(w, wa, res);
             }
             //util.print1DMatrix(w,features);
         }
@@ -63,26 +64,43 @@ void SGD::sgd() {
 
 void SGD::adamSGD() {
     Initializer initializer;
-
+    double *v = new double[features];
     wInit = initializer.initialWeights(features);
-    double* v = initializer.zeroWeights(features);
-    double* r = initializer.zeroWeights(features);
-    double* v1 = initializer.zeroWeights(features);
-    double* v2 = initializer.zeroWeights(features);
-    double* r1 = initializer.zeroWeights(features);
-    double* r2 = initializer.zeroWeights(features);
-    double* w1 = initializer.zeroWeights(features);
-    double* w2 = initializer.zeroWeights(features);
-    double* v_hat = initializer.zeroWeights(features);
-    double* r_hat = initializer.zeroWeights(features);
-    double* sq_r_hat = initializer.zeroWeights(features);
-    double* grad_mul = initializer.zeroWeights(features);
-    double* gradient = initializer.zeroWeights(features);
-    double* xiyi = initializer.zeroWeights(features);
-    double* w_xiyi = initializer.zeroWeights(features);
-    double* aw_axiyi = initializer.zeroWeights(features);
-    double* w1d = initializer.zeroWeights(features);
-    double* aw1 = initializer.zeroWeights(features);
+    initializer.initializeWeightsWithArray(features, v);
+    double *r = new double[features];
+    initializer.initializeWeightsWithArray(features, r);
+    double *v1 = new double[features];
+    initializer.initializeWeightsWithArray(features, v1);
+    double *v2 = new double[features];
+    initializer.initializeWeightsWithArray(features, v2);
+    double *r1 = new double[features];
+    initializer.initializeWeightsWithArray(features, v2);
+    double *r2 = new double[features];
+    initializer.initializeWeightsWithArray(features, v2);
+    double *w1 = new double[features];
+    initializer.initializeWeightsWithArray(features, w1);
+    double *w2 = new double[features];
+    initializer.initializeWeightsWithArray(features, w2);
+    double *v_hat = new double[features];
+    initializer.initializeWeightsWithArray(features, v_hat);
+    double *r_hat = new double[features];
+    initializer.initializeWeightsWithArray(features, r_hat);
+    double *sq_r_hat = new double[features];
+    initializer.initializeWeightsWithArray(features, sq_r_hat);
+    double *grad_mul = new double[features];
+    initializer.initializeWeightsWithArray(features, grad_mul);
+    double *gradient = new double[features];
+    initializer.initializeWeightsWithArray(features, gradient);
+    double *xiyi = new double[features];
+    initializer.initializeWeightsWithArray(features, xiyi);
+    double *w_xiyi = new double[features];
+    initializer.initializeWeightsWithArray(features, w_xiyi);
+    double *aw_axiyi = new double[features];
+    initializer.initializeWeightsWithArray(features, aw_axiyi);
+    double *w1d = new double[features];
+    initializer.initializeWeightsWithArray(features, w1d);
+    double *aw1 = new double[features];
+    initializer.initializeWeightsWithArray(features, aw1);
     double epsilon = 0.00000001;
     Util util;
     cout << "Training Samples : " << trainingSamples << endl;
@@ -92,10 +110,10 @@ void SGD::adamSGD() {
     //util.print1DMatrix(r, features);
 
     Matrix1 matrix(features);
-    double* res = new double[features];
+    double *res = new double[features];
     w = wInit;
     for (int i = 1; i < iterations; ++i) {
-        if(i % 10 == 0) {
+        if (i % 10 == 0) {
             //cout << "+++++++++++++++++++++++++++++++++" << endl;
             //util.print1DMatrix(w, features);
             //cout << "+++++++++++++++++++++++++++++++++" << endl;
@@ -106,28 +124,27 @@ void SGD::adamSGD() {
             double yixiw = matrix.dot(X[j], w);
             yixiw = yixiw * y[j];
 
-            double coefficient = 1.0 /(1.0 + double(i));
+            double coefficient = 1.0 / (1.0 + double(i));
 
-            if(yixiw<1) {
-                matrix.scalarAddition(X[j],y[j],xiyi);
+            if (yixiw < 1) {
+                matrix.scalarAddition(X[j], y[j], xiyi);
                 matrix.subtract(w, xiyi, w_xiyi);
                 matrix.scalarMultiply(w_xiyi, alpha, gradient);
 
             } else {
-                matrix.scalarMultiply(w,(1-alpha), gradient);
+                matrix.scalarMultiply(w, (1 - alpha), gradient);
             }
 
 
-
             matrix.scalarMultiply(v, beta1, v1);
-            matrix.scalarMultiply(gradient, (1-beta1), v2);
+            matrix.scalarMultiply(gradient, (1 - beta1), v2);
             matrix.add(v1, v2, v);
-            matrix.scalarMultiply(v, (1.0 /(1.0-(pow(beta1,(double)i)))), v_hat);
+            matrix.scalarMultiply(v, (1.0 / (1.0 - (pow(beta1, (double) i)))), v_hat);
             matrix.inner(gradient, gradient, grad_mul);
             matrix.scalarMultiply(r, beta2, r1);
-            matrix.scalarMultiply(grad_mul, (1-beta2), r2);
+            matrix.scalarMultiply(grad_mul, (1 - beta2), r2);
             matrix.add(r1, r2, r);
-            matrix.scalarMultiply(r, (1.0/(1.0-(pow(beta2,(double)i)))), r_hat);
+            matrix.scalarMultiply(r, (1.0 / (1.0 - (pow(beta2, (double) i)))), r_hat);
             //w1 = matrix.sqrt(r_hat);
             //w1 = matrix.scalarAddition(w1,epsilon);
             //w1 = matrix.divide(v_hat,w1);
@@ -144,12 +161,12 @@ void SGD::adamSGD() {
 
     cout << "============================================" << endl;
     printf("Final Weight\n");
-    util.print1DMatrix(w,features);
+    util.print1DMatrix(w, features);
     this->setWFinal(w);
     cout << "============================================" << endl;
 }
 
-double* SGD::getW() const {
+double *SGD::getW() const {
     return w;
 }
 
