b006fc4dae46a6774996beb255797161c8b79beb
vibhatha <vibhatha@gmail.com>
2018-12-28 14:13:36 -0500

    testing cost function

--- SGD.cpp
+++ SGD.cpp
@@ -810,7 +810,7 @@ void SGD::pegasosBlockSgd(double *w, string summarylogfile, string epochlogfile,
         Predict predict(Xtest, ytest, w , testingSamples, features);
         //cost = cost_sum / trainingSamples;
         //cost_sum = 0;
-        cost =  (0.5 * alpha * fabs(matrix.dot(w,w))) + max(0.0, (1-yixiw));
+        cost =  (0.5 * alpha * fabs(matrix.dot(w,w))/ trainingSamples) + max(0.0, (1-yixiw));
         acc = predict.predict();
         cout  << "Block Size:  "<< block_size << ", Pegasos Block SGD Epoch " << i << " Testing Accuracy : " << acc << "%" << ", Hinge Loss : " << cost << ", Count : " << count << endl;
 
