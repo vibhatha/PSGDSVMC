b5804f6435dad314c5555649839be95fd6975c5b
Vibhatha Abeykoon <vibhatha@gmail.com>
2019-01-04 21:38:00 -0500

    testing sequential block model

--- SGD.cpp
+++ SGD.cpp
@@ -812,7 +812,7 @@ void SGD::pegasosBlockSgd(double *w, string summarylogfile, string epochlogfile,
         //cost_sum = 0;
         cost =  (0.5 * alpha * fabs(matrix.dot(w,w))) + max(0.0, (1-yixiw));
         acc = predict.predict();
-        cout  << "Block Size:  "<< block_size << ", Pegasos Block SGD Epoch " << i << " Testing Accuracy : " << acc << "%" << ", Hinge Loss : " << cost << ", Count : " << count << endl;
+        cout  << "Block Size:  "<< block_size << ", Pegasos Block SGD Epoch " << i << " Testing Accuracy : " << acc << "%" << ", Hinge Loss : " << cost << ", Count : " << count << "/" << trainingSamples << endl;
 
         util.writeAccuracyPerEpoch(i, acc, epochlogfile);
         i++;
