58d59a07e333a76e07481e0cd9a5488edd5e8e21
vibhatha <vibhatha@gmail.com>
2019-07-01 16:56:24 -0400

    adding updated classes

--- refactor_block_sequential.py
+++ refactor_block_sequential.py
@@ -1,8 +1,9 @@
 import os
 import sys
 
-base_path = "/home/vibhatha/Documents/Research/logs/psgsvmc/2019_01_25/block_sequential/epochlogs/webspam/"
+base_path = "/home/vibhatha/Documents/Research/logs/psgsvmc/2019_02_18/20/ijcnn1/ijcnn1_2019_02_20_seq_block_epochlog_5/ijcnn1/all/"
 comm_gaps = [1,2,4,8,16,32,64,128,256,512,1024,2048,4096]
+comm_gaps = [1,2,4,8,512,1024]
 dataset = "webspam"
 
 list = os.listdir(base_path)
